bb46a2a9f044de8397015dd784a51e17b94ab216
martigallego <marti.gallego01@estudiant.upf.edu>
2026-01-10 12:09:36 +0100

    Update application.h
    prova 1 de push

--- src/framework/application.h
+++ src/framework/application.h
@@ -2,6 +2,7 @@
 	+ This class encapsulates the application, is in charge of creating the data, getting the user input, process the update and render.
 */
 
+//PROVA 1 DE PUSH
 #pragma once
 
 #include "main/includes.h"
